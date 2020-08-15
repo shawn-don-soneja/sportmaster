14b7f4ff851cc52cac0b4b21df4f34d143410758
owner <owner@owners-MacBook-Air.local>
2020-08-15 10:50:06 -0400

    changes to js file, adjusting syntax

--- dev/shopify.js
+++ dev/shopify.js
@@ -2,7 +2,7 @@ import Client from 'shopify-buy';
 
 // Initializing a client to return content in the store's primary language
 const client = Client.buildClient({
-  domain: 'https://sportmaster-bait-and-tackle.myshopify.com/'',
+  domain: 'https://sportmaster-bait-and-tackle.myshopify.com/',
   storefrontAccessToken: 'aad75c99c6746d652cf7b6fc714a0872'
 });
 
